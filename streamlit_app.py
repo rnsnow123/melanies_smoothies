03aa09dc6183a50a57ee21cb2d4a04431b9f7dd5
rnsnow123 <dvs489@yahoo.com>
2024-08-20 22:38:18 -0400

    Update streamlit_app.py
    Search On Added Back

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,11 @@ if ingredients_list:
     name_on_order = title
     
     for fruit_chosen in ingredients_list:
-        search_on_value = session.table("smoothies.public.fruit_options").filter(col('FRUIT_NAME') == fruit_chosen).select(col('SEARCH_ON')).collect()[0]['SEARCH_ON']
         ingredients_string += fruit_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information ')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on_value)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
