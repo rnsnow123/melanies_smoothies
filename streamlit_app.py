e9d5f38073dbe9d30eba1f557ba8c83225b1e0e8
rnsnow123 <dvs489@yahoo.com>
2024-08-20 22:24:43 -0400

    Update streamlit_app.py
    Quotation Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write("The name on your Smoothie will be", title)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -33,7 +33,7 @@ if ingredients_list:
         search_on_value = session.table("smoothies.public.fruit_options").filter(col('FRUIT_NAME') == fruit_chosen).select(col('SEARCH_ON')).collect()[0]['SEARCH_ON']
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information ')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/search_on_value)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on_value)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
