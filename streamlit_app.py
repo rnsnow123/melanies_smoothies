3fbb34ac77eea4b9ef64f88211860bd89d870787
rnsnow123 <dvs489@yahoo.com>
2024-08-20 21:58:46 -0400

    Update streamlit_app.py
    Updated Fruit Chosen Block

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -48,6 +50,3 @@ if ingredients_list:
        st.success('Your Smoothie is ordered ' + name_on_order + '!',icon="✅")
         
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
